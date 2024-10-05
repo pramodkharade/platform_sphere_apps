94ad9b8db2e31903fc1d309654976f045a5aef25
Vishesh Jindal <ece.vishesh@gmail.com>
2024-10-06 01:11:07 +0530

    import refactor

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,7 +1,6 @@
+import authReducer from '@features/auth/authSlice';
+import postFeedReducer from '@features/home/postFeedSlice';
 import { combineReducers } from '@reduxjs/toolkit';
-
-import authReducer from '../features/auth/authSlice';
-import postFeedReducer from '../features/home/postFeedSlice';
 
 const rootReducer = combineReducers({
   auth: authReducer,
